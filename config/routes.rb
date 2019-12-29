4086191a0a6b1894578efdcfd31338269a5a4798
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-29 15:50:44 -0700

    Add routes to pass RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :dogs do
+    resources :tricks, only: [:index, :new, :create]
+  end
+
+  get '/hello', to: 'nonrestful#hello'
+  get '/greetings/:name', to: 'nonrestful#what_is_your_name'
+  patch '/items/:the_item_id/deactivate', to: 'nonrestful#deactivate'
+  delete '/subtract/:x/from/:y', to: 'nonrestful#subtract', as: 'subtract'
+
+  namespace :admin do
+    resources :users, only: [:show]
+    resources :dogs, only: [:index] do
+      resources :tricks, only: [:index]
+    end
+    resources :tricks, only: [:destroy]
+  end
 end
